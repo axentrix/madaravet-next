fcb6e888e09366340bd3e019c33478bdb2aaab97
Anna Notovska <annanotovska@gmail.com>
2025-11-04 15:55:38 +0200

    name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,15 +23,8 @@ export default function Footer() {
         <a href="https://www.instagram.com/madaravet/"><img suppressHydrationWarning src="/images/instagram.svg" alt="social_instagram" data-i18n-alt="social_instagram" className="w-6 h-6" /></a>
         <a href="https://wa.me/359888198585"><img suppressHydrationWarning src="/images/whatapp.svg" alt="social_whatsapp" data-i18n-alt="social_whatsapp" className="w-6 h-6" /></a>
       </div>
-      <Link href="https://www.madaravet.com" aria-label="Home" className="inline-block">
-        <img src="/images/logo_white.svg" alt="Logo White" className="mx-auto w-32 mb-6" />
-      </Link>
-      <div className="flex justify-center gap-6 mb-4">
-        <a href="https://www.facebook.com/madaravet"><img suppressHydrationWarning src="/images/fb.svg" alt="social_facebook" data-i18n-alt="social_facebook" className="w-6 h-6" /></a>
-        <a href="https://www.instagram.com/madaravet/"><img suppressHydrationWarning src="/images/instagram.svg" alt="social_instagram" data-i18n-alt="social_instagram" className="w-6 h-6" /></a>
-        <a href="https://wa.me/359888198585"><img suppressHydrationWarning src="/images/whatapp.svg" alt="social_whatsapp" data-i18n-alt="social_whatsapp" className="w-6 h-6" /></a>
-      </div>
-      <p suppressHydrationWarning className="text-lg" data-i18n="photos">photos</p>
+    
+      <a suppressHydrationWarning href="https://www.facebook.com/p/Fotorismi-100063472399562/" className="text-lg" data-i18n="photos">photos</a>
       <p suppressHydrationWarning className="text-lg" data-i18n="copyright">copyright</p>
 
     </footer>
